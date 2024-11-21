09aa483b233cc20ce5daffa2b510d7f44e2f073d
Thomas Barzellino <thomas.barzellino@gmail.com>
2024-11-21 09:59:58 +0100

    adding renamed files

---
+++ testing/unit_testing_best_practice/test/test_best_practice_sample.py
@@ -0,0 +1,11 @@
+import sys
+# Always run from unit_testing_best_practice/test
+sys.path += ['C:/Users/thoma/Documents/DSTI/MLOps/DSTI_MLOps/testing/unit_testing_best_practice/src'] 
+
+from sample import *
+
+def test_answer():
+    assert func(4) == 5
+
+def test_answer_2():
+    assert func_2(3) == 5
